6a2269710187e3dc236eb3a964d39859f2fff207
aluno <samuelparacontato@gmail.com>
2019-10-29 16:11:20 -0300

    ...

--- src/externals/lambda.external.ts
+++ src/externals/lambda.external.ts
@@ -1,8 +1,10 @@
 const AWS = require('aws-sdk');
+const awsXRay = require('aws-xray-sdk');
+const awsSdk = awsXRay.captureAWS(AWS);
 
 export default class LambdaExternal {
   public static async call(functionName: string, payload?): Promise<Array<any>> {
-    const lambda = new AWS.Lambda({
+    const lambda = new awsSdk.Lambda({
       region: 'us-east-1'
     });
     const params = {
@@ -12,21 +14,17 @@ export default class LambdaExternal {
       Payload: payload ? JSON.stringify(payload) : undefined
     };
 
-    debugger;
     return new Promise<Array<any>>((resolve, reject) => {
-        debugger;
         lambda.invoke(params, (err, data) => {
         if (err) {
           console.log(err);
           return reject(err);
         }
-        debugger;
+
         const ans = JSON.parse(data.Payload);
-        debugger;
         if (ans.statusCode === 200) {
           return resolve(JSON.parse(ans.body));
         }
-        debugger;
         return reject();
       });
     });
